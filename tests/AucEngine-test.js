e2d8edfad9a384b267255ec76016f3b3e78c00cc
Postich-prog <mslutskikh@bk.ru>
2024-09-04 16:03:05 +0300

    add more tests

--- tests/AucEngine-test.js
+++ tests/AucEngine-test.js
@@ -21,4 +21,63 @@ describe("AucEngine", function () {
         console.log(currentOwner)
         expect(currentOwner).to.eq(owner.address)
     })
-})
+
+    async function getTimestamp(bn) {
+        return (
+            await ethers.provider.getBlock(bn)
+        ).timestamp
+    }
+
+    describe("createAuction", function () {
+        it("creates auction correctly", async function() {
+            const duration = 60
+            const tx = await auct.createAuction(
+                ethers.utils.parseEther("0.0001"),
+                3,
+                "fake item",
+                duration
+            )
+
+            const cAuction = await auct.auctions(0) // Promise
+            console.log(cAuction)
+            expect(cAuction.item).to.eq("fake item")
+            const ts = await getTimestamp(tx.blockNumber)
+            expect(cAuction.endsAt).to.eq(ts + duration)
+        })
+    })
+
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+
+    describe("buy", function () {
+        it("allows to buy", async function() {
+            await auct.connect(seller).createAuction(
+                ethers.utils.parseEther("0.0001"),
+                3,
+                "fake item",
+                60
+            )
+
+            this.timeout(5000) // 5s
+            await delay(1000) // 1s
+
+            const buyTx = await auct.connect(buyer).
+                buy(0, {value: ethers.utils.parseEther("0.0001")})
+
+            const cAuction = await auct.auctions(0)
+            const finalPrice = cAuction.finalPrice
+            await expect(() => buyTx).
+                to.changeEtherBalance(seller, finalPrice - Math.floor((finalPrice * 10) / 100))
+
+            await expect(buyTx)
+                .to.emit(auct, 'AuctionEnded')
+                .withArgs(0, finalPrice, buyer.address)
+
+            await expect(
+                auct.connect(buyer).
+                    buy(0, {value: ethers.utils.parseEther("0.0001")})
+            ).to.be.revertedWith('stopped!')
+        })
+    })
+}) 
